cb8f5a7f56035d5a027fc9a5311936c3e1ae4a9d
i.gorovoi <ivangorovoyyy@gmail.com>
2024-11-14 21:59:38 +0700

    fix: Исправили валидацию celebrate в router/cards

--- src/routes/cards.ts
+++ src/routes/cards.ts
@@ -16,26 +16,25 @@ router.get('/', getCards);
 router.post('/', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required(),
-    link: Joi.string().required(),
-    owner: Joi.string().required(),
+    link: Joi.string().required().uri(),
   }),
 }), createCard);
 
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().alphanum().length(24),
   }),
 }), deleteCard);
 
 router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().alphanum().length(24),
   }),
 }), likeCard);
 
 router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().required(),
+    cardId: Joi.string().required().alphanum().length(24),
   }),
 }), dislikeCard);
 
